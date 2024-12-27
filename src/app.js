c362e07c9eb78f23f4c547c0fe54dbf4d88a4840
Prithvi Raj <prithvi07raj07@gmail,com>
2024-12-27 16:49:24 +0530

    install nodemon

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("Starting app.js");
+const express=require('express');
+const app=express();
+const port=3000;
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+    res.end();
+})
+
+app.use("/user",(req,res)=>{
+    res.send('User page is created');
+    res.end();
+
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
