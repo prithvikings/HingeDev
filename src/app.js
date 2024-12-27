1d157b66a09507ae8bbc02e1fed7e0125869bfa6
Prithvi Raj <prithvi07raj07@gmail.com>
2024-12-28 00:18:52 +0530

    update package.json and src/app.js to modify dev script path and enhance request handling

--- src/app.js
+++ src/app.js
@@ -1,16 +1,44 @@
 const express=require('express');
 const app=express();
 const port=3000;
+
+
+
+//use will handle all type of request like get,post,put,delete etc other will not handle because its order is first
+// app.use('/',(req,res)=>{
+//     res.send('Hello World');
+//     res.end();
+// })
+
+//get will only handle get request 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.send('Hello World paaji');
     res.end();
 })
 
+app.post('/',(req,res)=>{
+    console.log('Post request is made');
+    res.send('Save the data');
+    res.end();
+})
+
+app.delete('/',(req,res)=>{
+    console.log('Delete request is made');
+    res.send('Delete the data');
+    res.end();
+})
+
+//use will handle all type of request like get,post,put,delete etc
 app.use("/user",(req,res)=>{
     res.send('User page is created');
     res.end();
-    
 })
+
+app.use("/admin",(req,res)=>{
+    res.send('Admin page is created');
+    res.end();
+})
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
