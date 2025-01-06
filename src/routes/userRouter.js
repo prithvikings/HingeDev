c09f53f4a60204be320b6ef0466a84427b9d1266
Prithvi Raj <prithvi07raj07@gmail.com>
2025-01-07 00:08:26 +0530

    Enhance userRouter: add connection requests endpoint and improve user data population

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,7 +12,22 @@ userRouter.get("/user/requests/pending", authenticate,async (req, res) => {
         const connectionRequests = await ConnectionRequestModel.find({
             toUserId:loggedinUser._id,
             status:"interested"
-        }).populate('fromUserId');
+        }).populate('fromUserId',['firstName','lastName','email']); 
+        res.json(connectionRequests);
+    } catch (error) {
+        return res.status(400).send({ error: error.message });
+    }
+});
+
+userRouter.get("/user/requests/connection", authenticate,async (req, res) => {
+    try {
+        const loggedinUser = req.user;
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or:[
+                {fromUserId:loggedinUser._id,status:"accepted"},
+                {toUserId:loggedinUser._id,status:"accepted"}
+            ]
+        }).populate('fromUserId',['firstName','lastName','email']);
         res.json(connectionRequests);
     } catch (error) {
         return res.status(400).send({ error: error.message });
