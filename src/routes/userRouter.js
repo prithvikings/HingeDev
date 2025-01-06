31c80269873300e408c0d25e8557c8cae9887d0e
Prithvi Raj <prithvi07raj07@gmail.com>
2025-01-07 00:12:41 +0530

    Add endpoints to retrieve sent and accepted connection requests for the logged-in user

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -19,6 +19,21 @@ userRouter.get("/user/requests/pending", authenticate,async (req, res) => {
     }
 });
 
+//get all the sent requests connection request for the loggedin user
+userRouter.get("/user/requests/sent", authenticate,async (req, res) => {
+    try {
+        const loggedinUser = req.user;
+        const connectionRequests = await ConnectionRequestModel.find({
+            fromUserId:loggedinUser._id,
+            status:"interested"
+        }).populate('toUserId',['firstName','lastName','email']);
+        res.json(connectionRequests);
+    } catch (error) {
+        return res.status(400).send({ error: error.message });
+    }
+})
+
+//get all the accepted requests connection request for the loggedin user
 userRouter.get("/user/requests/connection", authenticate,async (req, res) => {
     try {
         const loggedinUser = req.user;
